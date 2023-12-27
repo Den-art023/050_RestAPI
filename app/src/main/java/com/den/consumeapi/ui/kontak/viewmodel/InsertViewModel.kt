3501f637350a909008da670b0d056736a730b996
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2023-12-28 03:09:51 +0700

    Deklarasi var insertKontakState

--- app/src/main/java/com/den/consumeapi/ui/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/den/consumeapi/ui/kontak/viewmodel/InsertViewModel.kt
@@ -1,6 +1,18 @@
 package com.den.consumeapi.ui.kontak.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.den.consumeapi.repository.KontakRepository
 
-class insertViewModel(private val kontakRepository: KontakRepository): ViewModel(){}
+class insertViewModel(private val kontakRepository: KontakRepository): ViewModel(){
+
+    var insertKontakState by mutableStateOf(InsertUiState())
+        private set
+
+
+
+
+}
+
