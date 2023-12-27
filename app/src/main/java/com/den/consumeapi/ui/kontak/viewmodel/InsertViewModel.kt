2c820361818f583c6884fbb6970a4384903cc461
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2023-12-28 03:00:03 +0700

    Membuat class untuk insert

---
+++ app/src/main/java/com/den/consumeapi/ui/kontak/viewmodel/InsertViewModel.kt
@@ -0,0 +1,6 @@
+package com.den.consumeapi.ui.kontak.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.den.consumeapi.repository.KontakRepository
+
+class insertViewModel(private val kontakRepository: KontakRepository): ViewModel(){}
